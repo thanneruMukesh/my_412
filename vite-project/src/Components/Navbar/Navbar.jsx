eec657eabc885fc1d46e294f7a4ea932456c8b74
Thanneru Mukesh <thannerumukesh.t@theswipewire.in>
2026-03-26 15:58:59 +0530

    react installed

---
+++ vite-project/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import { AiOutlineHome } from "react-icons/ai";
+import { BiUser } from "react-icons/bi";
+import { FaLaptopCode } from "react-icons/fa";
+import { GoChecklist } from "react-icons/go";
+import { BiMessageSquareDetail } from "react-icons/bi";
+
+const Navbar = () => {
+  return (
+    <nav>
+        <a href="#">
+            
+        </a>
+    </nav>
+  )
+}
+
+export default Navbar
